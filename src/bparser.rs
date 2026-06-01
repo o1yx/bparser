80a444999d407a5c68d05ee943b8e3c046d9e748
Шарыгин Алексей Владимирович <sharyginn.09@gmail.com>
2026-06-01 17:14:52 +0300

    fix: Добавлены lifetimes

--- src/bparser.rs
+++ src/bparser.rs
@@ -8,31 +8,31 @@ enum State {
     Payload,
 }
 
-struct Protocol {
-    prefix: &[u8],
+struct Protocol<'a> {
+    prefix: &'a [u8],
     prefix_size: u8,
     payload_size: u8,
 }
 
-pub struct Parser {
+struct Parser<'a> {
     buffer: [u8; BUFFER_SIZE],
     state: State,
-    protocol: &Protocol,
+    protocol: &'a Protocol<'a>,
     bytes_read: u8,
     bytes_need_read: u8,
     buffer_position: usize,
 }
 
-impl Protocol {
-    pub fn init(&self, prefix: &[u8], prefix_size: u8, payload_size: u8) {
+impl<'a> Protocol<'a> {
+    pub fn init(&mut self, prefix: &'a [u8], prefix_size: u8, payload_size: u8) {
         self.prefix = prefix;
         self.prefix_size = prefix_size;
         self.payload_size = payload_size;
     }
 }
 
-impl Parser {
-    pub fn init(&self, protocol: &Protocol) {
+impl<'a> Parser<'a> {
+    pub fn init(&mut self, protocol: &'a Protocol) {
         self.state = State::Prefix;
         self.protocol = protocol;
     }
